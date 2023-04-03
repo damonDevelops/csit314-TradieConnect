caae44de62e199c9a7711b4682b7e3847ab404e2
Damon O'Neil <don988@uowmail.edu.au>
2023-04-03 16:32:55 +1000

    restructure

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,33 +1,129 @@
-// import statements
 import * as React from "react";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
-import ProTip from "../src/ProTip";
-import Link from "../src/Link";
-import Copyright from "../src/Copyright";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Paper } from "@mui/material";
 
-export default function Index() {
+const theme = createTheme();
+
+export default function Developer() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Material UI - Next.js example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the Sign In page
-        </Link>
-        <br />
-        <Link href="/SignUp" color="secondary">
-          Go to the Sign Up page
-        </Link>
-        <br />
-        <Link href="/Dashboard" color="secondary">
-          Go to the Dashboard
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Developer Homepage
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Developer Homepage
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              A place for developers to access different links to the website.
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained" href="./Customer/Dashboard">
+                Customer Dashboard
+              </Button>
+              <Button variant="contained" href="./Service-Provider/Dashboard">
+                Tradie Dashboard
+              </Button>
+            </Stack>
+          </Container>
+        </Box>
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Grid container spacing={3}>
+            {/* Chart */}
+            <Grid item sm={4}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  height: 120,
+                }}
+              >
+                <Typography
+                  variant="h5"
+                  align="center"
+                  color="text.secondary"
+                  paragraph
+                >
+                  Sign Up
+                </Typography>
+                <Button
+                  variant="contained"
+                  align="center"
+                  href="./Common-Pages/SignUp"
+                >
+                  Sign Up
+                </Button>
+              </Paper>
+            </Grid>
+            <Grid item sm={4}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  height: 120,
+                }}
+              >
+                <Typography
+                  variant="h5"
+                  align="center"
+                  color="text.secondary"
+                  paragraph
+                >
+                  Sign In
+                </Typography>
+                <Button
+                  variant="contained"
+                  align="center"
+                  href="./Common-Pages/SignIn"
+                >
+                  Sign In
+                </Button>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
+    </ThemeProvider>
   );
 }
